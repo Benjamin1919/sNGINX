9cb3e186267cf0d9be96be0875b58bd6e48265f9
Benjamin Hermit <72666597+Benjamin1919@users.noreply.github.com>
2025-10-29 21:16:22 +0800

    Create get-ngx-ver.sh

---
+++ scripts/get-ngx-ver.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "Fetching latest version of Nginx"
+
+NGINX_VERSION=$(curl -s --connect-timeout 10 --retry 2 --retry-delay 5 "https://api.github.com/repos/nginx/nginx/releases/latest" | jq -r '.tag_name | sub("^release-"; "")')
+
+if [ -z "$NGINX_VERSION" ]; then
+  echo "❌ Failed to fetch Nginx latest version, fallback to 1.29.2"
+  NGINX_VERSION=1.29.2
+else
+  echo "✅ NGINX_VERSION=$NGINX_VERSION"
+fi
+
+echo "NGINX_VERSION=$NGINX_VERSION" >> "$GITHUB_ENV"
