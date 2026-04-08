fbe6215a3ab2f3558109eb3a06f415b0f2218ca8
Benjamin <72666597+Benjamin1919@users.noreply.github.com>
2026-04-08 19:18:33 +0800

    Update and rename get-ngx-latest-ver.sh to get-ngx-git-ver.sh

--- scripts/get-ngx-git-ver.sh
+++ scripts/get-ngx-git-ver.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Fetching latest version of Nginx"
+echo "Fetching latest version of Nginx from github ..."
 
 NGINX_VERSION=$(curl -s --connect-timeout 10 --retry 2 --retry-delay 5 "https://api.github.com/repos/nginx/nginx/releases/latest" | jq -r '.tag_name | sub("^release-"; "")')
 
 if [ -z "$NGINX_VERSION" ]; then
-  echo "❌ Failed to fetch Nginx latest version, fallback to 1.29.6"
-  NGINX_VERSION=1.29.6
+  echo "❌ Failed to fetch Nginx latest version, fallback to 1.29.8"
+  NGINX_VERSION=1.29.8
 else
   echo "✅ NGINX_VERSION=$NGINX_VERSION"
 fi
