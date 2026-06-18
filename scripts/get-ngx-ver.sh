5fc4b49d5bf93c08ffa7471752f33af3992a17a8
Benjamin <72666597+Benjamin1919@users.noreply.github.com>
2026-06-18 19:51:58 +0800

    Update get-ngx-ver.sh

--- scripts/get-ngx-ver.sh
+++ scripts/get-ngx-ver.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Fetching latest version of Nginx ..."
+echo "Fetching latest version of Nginx (mainline) ..."
 
 DOWNLOAD_PAGE=$(curl -s --connect-timeout 10 --retry 2 --retry-delay 5 "https://nginx.org/en/download.html")
 NGINX_VERSION=$(echo "$DOWNLOAD_PAGE" | grep -A 5 "Mainline version" | grep -oP 'nginx-\K[0-9]+\.[0-9]+\.[0-9]+' | head -n 1)
 
 if [ -z "$NGINX_VERSION" ]; then
-  echo "❌ Failed to fetch Nginx latest version, fallback to 1.29.8"
-  NGINX_VERSION=1.29.8
+  echo "❌ Failed to fetch Nginx latest version, fallback to 1.31.2"
+  NGINX_VERSION=1.31.2
 else
   echo "✅ NGINX_VERSION=$NGINX_VERSION"
 fi
